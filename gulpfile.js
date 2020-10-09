dcb1d1ce27ebf33ae5c47f5390e724237385df22
keishabien <keishabien@gmail.com>
2020-10-08 19:24:50 -0500

    browsersync is working

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,51 @@
 'use strict';
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var pipeConcat = require('pipe-concat');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const pipeConcat = require('pipe-concat');
+const del = require('del');
+const concat = require('gulp-concat');
+const browserSync = require('browser-sync').create();
 
 sass.compiler = require('node-sass');
 
+
 //style paths
-var sassFiles = 'assets/sass/*.scss',
+const sassFiles = 'assets/scss/*.scss',
     cssDest = './';
 
 gulp.task('styles', function () {
-    // return gulp.src(sassFiles)
-    //     .pipe(sass().on('error', sass.logError))
-    //     .pipe(concat('style.css'))
-    //     .pipe(gulp.dest(cssDest));
+    // const styles = pipeConcat(
+    return gulp.src(sassFiles)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(concat('style.css'))
+        .pipe(gulp.dest(cssDest))
+        .pipe(browserSync.stream());
+    // );
+    // styles.on('end',() => {
+    //     console.log('compile finished!')
+    // });
+    // return styles;
+});
 
-    var stream = pipeConcat(
-        gulp.src(sassFiles)
-            .pipe(sass().on('error', sass.logError))
-            .pipe(gulp.dest('dist'))
-    );
-    stream.on('end',() => {
-        console.log('compile finished!')
-    });
-    return stream;
+gulp.task('clean', () => {
+    return del([
+        'style.css'
+    ]);
 });
 
 gulp.task('watch', function () {
-    gulp.watch(sassFiles, gulp.series('styles'));
+    gulp.watch(sassFiles, gulp.series(['clean', 'styles']));
+});
+
+gulp.task('serve', function () {
+    browserSync.init({
+        proxy: "niteowl/"
+    });
+
+    gulp.watch(sassFiles, gulp.series(['clean', 'styles']));
+    // gulp.watch('./**/*.php').on('change', browserSync.reload);
 });
 
 // Default Task
-gulp.task('default', gulp.parallel('styles','watch'));
+gulp.task('default', gulp.parallel('clean', 'styles', 'watch', 'serve'));
