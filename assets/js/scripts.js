4df2c0825921be58e26fc53cdbf1e9716c408aa9
keishabien <keishabien@gmail.com>
2020-10-26 23:26:28 -0500

    updated animations, need to look at why under 600px gets 2 px of random width

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,10 +1,1 @@
-$(document).ready(function (){
-    console.log('test');
-    // // span#main.typer
-    // const typer = $(this).attr("typer-start");
-    // console.log($(".typer-start"));
-    // setTimeout(function (){
-    //     $(".typer").addClass("typer-start");
-    // }, 2000);
-});
 
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <head>
     <meta charset="UTF-8">
     <title>Nite Owl Productions</title>
@@ -46,11 +46,12 @@
                   data-cursor-display="&#9613;"></span>
         </h1>
     </main>
+    <div style="clear:both;"></div>
 </div>
 
 
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 <script async src="https://unpkg.com/typer-dot-js@0.1.0/typer.js"></script>
-<script src="scripts.js"></script>
+<!--<script src="scripts.js"></script>-->
 </body>
 </html>
